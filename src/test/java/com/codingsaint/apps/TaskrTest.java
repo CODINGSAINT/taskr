4f5e6e4fa69731919820a9b2d7e8158456d96104
Coding Saint <codingsaint@gmail.com>
2020-11-28 08:02:21 +0000

    Initial commit

---
+++ src/test/java/com/codingsaint/apps/TaskrTest.java
@@ -0,0 +1,21 @@
+package com.codingsaint.apps;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import javax.inject.Inject;
+
+@MicronautTest
+public class TaskrTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
